c1c0a33a6df28e52589d2101ff167701b584f485
Triệu Hoàng Thiên Ân <thienan2003bt@gmail.com>
2025-10-25 22:29:32 +0700

    Reusable AuthForm

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,8 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignInPage = () => {
-  return <div>SignInPage</div>;
+  return <AuthForm type="sign-in" />;
 };
 
 export default SignInPage;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,8 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return <AuthForm type="sign-up" />;
 };
 
 export default SignUpPage;
